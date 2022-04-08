3ec5bf18f78621eae912e3531fd6888fa47c53f6
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-04-08 12:30:41 -0300

    Initial commit

---
+++ src/api/api.js
@@ -0,0 +1,49 @@
+export const Api = {
+	baseUrl: "http://localhost:3001",
+
+	createUrl: () => Api.baseUrl + "/personagens",
+
+	readAllUrl: () => Api.baseUrl + "/personagens",
+	readSingleUrl: (id) => Api.baseUrl + "/personagens/" + id,
+
+	updateUrl: (id) => Api.baseUrl + "/personagens/" + id,
+
+	deleteUrl: (id) => Api.baseUrl + "/personagens/" + id,
+	deleteAllUrl: () => Api.baseUrl + "/",
+
+	// Create
+	buildApiPostRequest: (url, body) => {
+		return fetch(url, {
+			method: "POST",
+			headers: new Headers({
+				"Content-type": "application/json",
+			}),
+			body: JSON.stringify(body),
+		});
+	},
+
+	// ReadAll
+	buildApiGetRequest: (url) => {
+		return fetch(url, {
+			method: "GET",
+		});
+	},
+
+	// UpdateById
+	buildApiPutRequest: (url, body) => {
+		return fetch(url, {
+			method: "PUT",
+			headers: new Headers({
+				"Content-type": "application/json",
+			}),
+			body: JSON.stringify(body),
+		});
+	},
+
+	// DeleteAll
+	buildApiDeleteRequest: (url) => {
+		return fetch(url, {
+			method: "DELETE",
+		});
+	},
+};
