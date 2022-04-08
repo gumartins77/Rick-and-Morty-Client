f9db621692ea8dd749e05b6dbbf126429701efb8
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-04-08 18:27:28 -0300

    feat(register):create register function base

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import Button from '../../components/button/Button';
-import Fieldset from '../../components/fieldset/Fieldset';
-import Form from '../../components/form/Form';
-import Input from '../../components/input/Input';
-import { useNavigate } from 'react-router-dom';
-import './Register.css';
+import React from "react";
+import Button from "../../components/button/Button";
+import Fieldset from "../../components/fieldset/Fieldset";
+import Form from "../../components/form/Form";
+import Input from "../../components/input/Input";
+import { useNavigate } from "react-router-dom";
+import "./Register.css";
 
-export default function Register(){
-    const navigate = useNavigate();
-    return (
-        <section className='page login'>
-            <Form>
-                <img src='/assets/logo-blue-croped.gif' alt='logo blueEdtech'></img>
-                <Fieldset>
-                    <Input type={"name"} name={"inputName"} description={"Name"} />
-                    <Input type={"email"} name={"inputEmail"} description={"Email"} />
-                    <Input type={"password"} name={"inputPassword"} description={"Password"}/>
-                    <Button description={"Register"} />
-                    <Button description={"Cancel"} className="btn-red"  onClick={(event)=>{event.preventDefault(); navigate('/')}}/>
-                </Fieldset>
-            </Form>
-        </section >
-    );
+export default function Register() {
+  const navigate = useNavigate();
+
+  const register = (event)=>{
+    event.preventDefault();
+    const preload = {
+        name:event.target.inputName.value,
+        email:event.target.inputEmail.value,
+        password:event.target.inputPassword.value
+    }
+    console.log(preload);
+  }
+
+  return (
+    <section className="page login">
+      <Form onSubmit={register}>
+        <img src="/assets/logo-blue-croped.gif" alt="logo blueEdtech"></img>
+        <Fieldset>
+          <Input type={"name"} name={"inputName"} description={"Name"} />
+          <Input type={"email"} name={"inputEmail"} description={"Email"} />
+          <Input
+            type={"password"}
+            name={"inputPassword"}
+            description={"Password"}
+          />
+          <Button description={"Register"} />
+          <Button
+            description={"Cancel"}
+            className="btn-red"
+            onClick={(event) => {
+              event.preventDefault();
+              navigate("/");
+            }}
+          />
+        </Fieldset>
+      </Form>
+    </section>
+  );
 }
